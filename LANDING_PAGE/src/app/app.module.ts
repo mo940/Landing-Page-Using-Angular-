8274f9124ed0c645e833a36a802af01d592f0919
moren maher <moren.maher234@gmail.com>
2024-11-16 13:35:46 +0200

    complete home component

--- LANDING_PAGE/src/app/app.module.ts
+++ LANDING_PAGE/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { DataComponent } from './data/data.component';
+import { FooterComponent } from './footer/footer.component';
+import { PriceComponent } from './price/price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    DataComponent,
+    FooterComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
